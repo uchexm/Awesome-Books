38c6e2b1ec41c8989c2176374bc0ff6bc7fe68d6
uchexm <109084069+uchexm@users.noreply.github.com>
2022-11-29 12:59:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const body = document.querySelector('.body-1');
-let bookno= 1;
+let bookno = 1;
 let bookLists = [
 ];
 
 function printfn() {
   body.replaceChildren();
   const localbooks = JSON.parse(localStorage.getItem('books'));
-  bookLists=localbooks;
-  localbooks.forEach(book => {
+  bookLists = localbooks;
+  localbooks.forEach((book) => {
     const newDiv = document.createElement('div');
     newDiv.innerHTML = `
     <p>${book.Title}</p>
@@ -16,12 +16,10 @@ function printfn() {
     <hr>
     `;
     body.append(newDiv);
-  
   });
-
 }
 
-if (localStorage.getItem('books')){
+if (localStorage.getItem('books')) {
   printfn();
 }
 
@@ -30,26 +28,27 @@ const title = document.getElementById('title');
 const author = document.getElementById('author');
 
 addBtn.addEventListener('click', () => {
-  if (title.value!='' && author.value!='') {
-  bookLists.push(
-  {
-    id: 'book'+bookno,
-    Title: title.value,
-    Author: author.value
-  });
-  localStorage.setItem('books', JSON.stringify(bookLists));
-  printfn();
-  bookno+=1;
-  document.forms[0].reset();
-}
+  if (title.value !== '' && author.value !== '') {
+    bookLists.push(
+      {
+        id: `book${bookno}`,
+        Title: title.value,
+        Author: author.value,
+      },
+    );
+    localStorage.setItem('books', JSON.stringify(bookLists));
+    printfn();
+    bookno += 1;
+    document.forms[0].reset();
+  }
 });
 
-document.addEventListener('click', removed=>{
- const {id} = removed.target;
- if (id.substr(0,4)=== 'book'){
-  const item = document.getElementById(id);
-  item.parentElement.remove();
-  bookLists= bookLists.filter(b=> b.id !== id);
-  localStorage.setItem('books', JSON.stringify(bookLists));
- }
-})
+document.addEventListener('click', (removed) => {
+  const { id } = removed.target;
+  if (id.substr(0, 4) === 'book') {
+    const item = document.getElementById(id);
+    item.parentElement.remove();
+    bookLists = bookLists.filter((b) => b.id !== id);
+    localStorage.setItem('books', JSON.stringify(bookLists));
+  }
+});
