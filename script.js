894e859578bce2a8e04711991969bb3437a16f9b
uchexm <109084069+uchexm@users.noreply.github.com>
2022-11-30 13:13:25 +0200

    Added some style to the Awesome books

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-class bookdata {
-  constructor(id, title, author){
-    this.Id= id;
-    this.Title= title;
-    this.Author= author;
+// eslint-disable-next-line max-classes-per-file
+class Bookdata {
+  constructor(id, title, author) {
+    this.Id = id;
+    this.Title = title;
+    this.Author = author;
   }
 }
 
-class books{
+class Books {
   bookLists = [
   ];
+
   bookno = 1;
 
-  constructor(book){
-    this.Book= book;
+  constructor(book) {
+    this.Book = book;
   }
 
   printfn() {
@@ -20,41 +22,41 @@ class books{
     body.replaceChildren();
     const localbooks = JSON.parse(localStorage.getItem('books'));
     this.bookLists = localbooks;
+    let index = 1;
     localbooks.forEach((book) => {
       const newDiv = document.createElement('div');
       newDiv.innerHTML = `
-      <p>${book.Title}</p>
-      <p>${book.Author}</p>
-      <button type="button" id=${book.Id} value="Remove">Remove</button>
-      <hr>
+      <p>"${book.Title}" by ${book.Author}</p>
+      <button type="button" class="remove" id=${book.Id} value="Remove">Remove</button>
       `;
+      if (index % 2 !== 0) {
+        newDiv.classList.add('blue');
+      }
+      newDiv.classList.add('book-line');
       body.append(newDiv);
+      index += 1;
     });
   }
 
-  addbooks(title, author){
-    this.Book = new bookdata(`book${this.bookno}`, title, author);
+  addbooks(title, author) {
+    this.Book = new Bookdata(`book${this.bookno}`, title, author);
     this.bookLists.push(
-      this.Book
+      this.Book,
     );
     localStorage.setItem('books', JSON.stringify(this.bookLists));
     this.printfn();
     this.bookno += 1;
   }
 
-  deletebook(id){
+  deletebook(id) {
     const item = document.getElementById(id);
     item.parentElement.remove();
     this.bookLists = this.bookLists.filter((b) => b.Id !== id);
     localStorage.setItem('books', JSON.stringify(this.bookLists));
   }
-
-
 }
 
-
-const bookstore = new books();
-
+const bookstore = new Books();
 
 if (localStorage.getItem('books')) {
   bookstore.printfn();
@@ -66,7 +68,7 @@ const author = document.getElementById('author');
 
 addBtn.addEventListener('click', () => {
   if (title.value !== '' && author.value !== '') {
-    bookstore.addbooks(title.value,author.value);
+    bookstore.addbooks(title.value, author.value);
     document.forms[0].reset();
   }
 });
@@ -74,6 +76,6 @@ addBtn.addEventListener('click', () => {
 document.addEventListener('click', (removed) => {
   const { id } = removed.target;
   if (id.substr(0, 4) === 'book') {
-   bookstore.deletebook(id); 
+    bookstore.deletebook(id);
   }
 });
