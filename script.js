b074d0fc546bb414b7281a9e7275e0a6c6ee0525
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-11-30 11:41:42 +0200

    refactored using classes

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-const body = document.querySelector('.body-1');
-let bookno = 1;
-let bookLists = [
-];
-
-function printfn() {
-  body.replaceChildren();
-  const localbooks = JSON.parse(localStorage.getItem('books'));
-  bookLists = localbooks;
-  localbooks.forEach((book) => {
-    const newDiv = document.createElement('div');
-    newDiv.innerHTML = `
-    <p>${book.Title}</p>
-    <p>${book.Author}</p>
-    <button type="button" id=${book.id} value="Remove">Remove</button>
-    <hr>
-    `;
-    body.append(newDiv);
-  });
+class bookdata {
+  constructor(id, title, author){
+    this.Id= id;
+    this.Title= title;
+    this.Author= author;
+  }
 }
 
+class books{
+  bookLists = [
+  ];
+  bookno = 1;
+
+  constructor(book){
+    this.Book= book;
+  }
+
+  printfn() {
+    const body = document.querySelector('.body-1');
+    body.replaceChildren();
+    const localbooks = JSON.parse(localStorage.getItem('books'));
+    this.bookLists = localbooks;
+    localbooks.forEach((book) => {
+      const newDiv = document.createElement('div');
+      newDiv.innerHTML = `
+      <p>${book.Title}</p>
+      <p>${book.Author}</p>
+      <button type="button" id=${book.Id} value="Remove">Remove</button>
+      <hr>
+      `;
+      body.append(newDiv);
+    });
+  }
+
+  addbooks(title, author){
+    this.Book = new bookdata(`book${this.bookno}`, title, author);
+    this.bookLists.push(
+      this.Book
+    );
+    localStorage.setItem('books', JSON.stringify(this.bookLists));
+    this.printfn();
+    this.bookno += 1;
+  }
+
+  deletebook(id){
+    const item = document.getElementById(id);
+    item.parentElement.remove();
+    this.bookLists = this.bookLists.filter((b) => b.Id !== id);
+    localStorage.setItem('books', JSON.stringify(this.bookLists));
+  }
+
+
+}
+
+
+const bookstore = new books();
+
+
 if (localStorage.getItem('books')) {
-  printfn();
+  bookstore.printfn();
 }
 
 const addBtn = document.getElementById('btn');
@@ -29,16 +66,7 @@ const author = document.getElementById('author');
 
 addBtn.addEventListener('click', () => {
   if (title.value !== '' && author.value !== '') {
-    bookLists.push(
-      {
-        id: `book${bookno}`,
-        Title: title.value,
-        Author: author.value,
-      },
-    );
-    localStorage.setItem('books', JSON.stringify(bookLists));
-    printfn();
-    bookno += 1;
+    bookstore.addbooks(title.value,author.value);
     document.forms[0].reset();
   }
 });
@@ -46,9 +74,6 @@ addBtn.addEventListener('click', () => {
 document.addEventListener('click', (removed) => {
   const { id } = removed.target;
   if (id.substr(0, 4) === 'book') {
-    const item = document.getElementById(id);
-    item.parentElement.remove();
-    bookLists = bookLists.filter((b) => b.id !== id);
-    localStorage.setItem('books', JSON.stringify(bookLists));
+   bookstore.deletebook(id); 
   }
 });
