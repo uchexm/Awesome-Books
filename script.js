afdf2d66fa5473c0b674e0939d3e24e8569eaf73
uchexm <109084069+uchexm@users.noreply.github.com>
2022-12-02 10:08:58 +0200

    Add event listener

--- script.js
+++ script.js
@@ -49,10 +49,10 @@ class Books {
   }
 
   deletebook(id) {
-    const item = document.getElementById(id);
-    item.parentElement.remove();
+   
     this.bookLists = this.bookLists.filter((b) => b.Id !== id);
     localStorage.setItem('books', JSON.stringify(this.bookLists));
+    this.printfn();
   }
 }
 
@@ -79,19 +79,35 @@ document.addEventListener('click', (removed) => {
     bookstore.deletebook(id);
   }
 });
+document.getElementById('lis').classList.add('active');
+
+
+document.querySelector('.contact').style.display = 'none';
+  document.querySelector('.book').style.display = 'block';
+  document.querySelector('.add').style.display = 'none';
+
 document.querySelector('#con').addEventListener('click', () => {
+  document.getElementById('con').classList.add('active');
+  document.getElementById('lis').classList.remove('active');
+  document.getElementById('new').classList.remove('active');
   document.querySelector('.contact').style.display = 'block';
   document.querySelector('.book').style.display = 'none';
   document.querySelector('.add').style.display = 'none';
 });
 
 document.querySelector('#new').addEventListener('click', () => {
+  document.getElementById('new').classList.add('active');
+  document.getElementById('con').classList.remove('active');
+  document.getElementById('lis').classList.remove('active');
   document.querySelector('.contact').style.display = 'none';
   document.querySelector('.book').style.display = 'none';
   document.querySelector('.add').style.display = 'block';
 });
 
 document.querySelector('#lis').addEventListener('click', () => {
+  document.getElementById('lis').classList.add('active');
+  document.getElementById('con').classList.remove('active');
+  document.getElementById('new').classList.remove('active');
   document.querySelector('.contact').style.display = 'none';
   document.querySelector('.book').style.display = 'block';
   document.querySelector('.add').style.display = 'none';
